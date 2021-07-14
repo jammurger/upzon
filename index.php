f96e3f6adf53f13ca764cd0c356355441f9cc1c8
upzerk <86806059+upzerk@users.noreply.github.com>
2021-07-14 08:16:15 +0300

    Bootstrap js and css function called

--- index.php
+++ index.php
@@ -1,17 +1,14 @@
 <?php include 'config/config.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <?php echo  csjs(); ?>
   <title>upzon Data Analiysis</title>
 </head>
-
 <body>
-
   <section class="container">
     <table class="table">
       <tr>
@@ -53,6 +50,4 @@
     </table>
   </section>
 </body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
-
 </html>
