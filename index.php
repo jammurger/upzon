481e9068517889e73a46f3edc53286ec7e429dad
upzerk <86806059+upzerk@users.noreply.github.com>
2021-07-17 06:41:16 +0300

    added new warnings and  missed alt tags section

--- index.php
+++ index.php
@@ -1,13 +1,24 @@
 <?php include 'config/config.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php echo  csjs(); ?>
   <title>upzon Data Analiysis</title>
+  <style>
+    th {
+      background: white;
+      position: sticky;
+      top: 0;
+      /* Don't forget this, required for the stickiness */
+      box-shadow: 0 2px 2px -1px rgba(0, 0, 0, 0.4);
+    }
+  </style>
 </head>
+
 <body>
   <section class="container">
     <table class="table">
@@ -22,24 +33,49 @@
         <th>H4 Count</th>
         <th>H5 Count</th>
         <th>H6 Count</th>
+        <th>Image Alt Tag</th>
         <th>Analyzed Time</th>
         <th>Report</th>
       </tr>
       <?php
       $analiysis = $db->prepare("SELECT * from analiysis ORDER BY analiysis_id DESC");
       $analiysis->execute();
-      while ($show = $analiysis->fetch(PDO::FETCH_ASSOC)) { ?>
+      while ($show = $analiysis->fetch(PDO::FETCH_ASSOC)) {
+      ?>
         <tr>
           <td><?php echo $show['analiysis_id'] ?></td>
           <td><?php echo $show['analiysis_url'] ?></td>
           <td><?php echo $show['analiysis_title'] ?></td>
           <td><?php echo $show['analiysis_canonical'] ?></td>
-          <td><?php echo $show['analiysis_h1'] ?></td>
-          <td><?php echo $show['analiysis_h2'] ?></td>
+          <?php if ($show['analiysis_h1'] == 1) { ?>
+            <td class="text-success"><?php echo $show['analiysis_h1'] ?></td>
+          <?php } elseif ($show['analiysis_h1'] != 1) { ?>
+            <td class="bg-danger"><?php echo $show['analiysis_h1'] ?></td>
+          <?php } ?>
+          <?php if ($show['analiysis_h2'] == 0 and $show['analiysis_h3'] > $show['analiysis_h2']) { ?>
+            <td class="bg-danger"><?php echo $show['analiysis_h2'] ?></td>
+          <?php  } else { ?>
+            <td class="bg-success"><?php echo $show['analiysis_h2'] ?></td>
+          <?php  }
+          ?>
+
           <td><?php echo $show['analiysis_h3'] ?></td>
           <td><?php echo $show['analiysis_h4'] ?></td>
-          <td><?php echo $show['analiysis_h5'] ?></td>
+          <?php if ($show['analiysis_h5'] == 0 and $show['analiysis_h6'] > $show['analiysis_h5']) { ?>
+            <td class="bg-danger"><?php echo $show['analiysis_h5'] ?></td>
+          <?php   } else { ?>
+            <td class="bg-success"><?php echo $show['analiysis_h5'] ?></td>
+          <?php } ?>
+
           <td><?php echo $show['analiysis_h6'] ?></td>
+          <?php
+          $text = AltTextAnalyzer($show['analiysis_images'], $show['analiysis_imagesalt']);
+          if ($text["counter"] != 0) { ?>
+            <td class="bg-danger"> <?php echo $text["counter"] . " Missed Alt Tag"; ?> </td>
+          <?php  } else { ?>
+            <td class="bg-success">Good</td>
+          <?php  }
+          ?>
           <td>
             <?php $published = date_create($show['analiysis_time']);
             echo date_format($published, 'M d, Y'); ?>
@@ -48,7 +84,8 @@
         </tr>
       <?php } ?>
     </table>
-     <p>For more<a href="https://github.com/upzerk/upzon"> upzerk github </a>& <a href="https://upzerk.com/"> upzerk website.</a></p>
+    <p>For more <a href="https://github.com/upzerk/upzon">upzerk github </a></p>
   </section>
 </body>
+
 </html>
