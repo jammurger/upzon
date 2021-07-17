0323189aa57aaa10041146e9cff730d0d8fe5101
upzerk <86806059+upzerk@users.noreply.github.com>
2021-07-17 06:46:13 +0300

    Update function.php

--- config/function.php
+++ config/function.php
@@ -1,4 +1,6 @@
 <?php
+
+
 // Cleaned json with this way :D I didn't afford for finding normal way looking good imo
 function tagcleaner($data)
 {
@@ -9,4 +11,33 @@ function tagcleaner($data)
     $data = str_replace('"', "", $data);
     return $data;
 }
-?>
+
+
+function AltTextAnalyzer($ImageList,$AltTextList) {
+    $search_map = array('["','"]');
+    $replace_map = array('','');
+    $cleaned_Images_List = str_replace($search_map,$replace_map,$ImageList);
+    $cleaned_Images_Array = explode('", "',$cleaned_Images_List);
+    //var_dump($cleaned_Images_Array);
+    $cleaned_AltText_List = str_replace($search_map,$replace_map,$AltTextList);
+    $cleaned_AltText_Array = explode('", "',$cleaned_AltText_List);
+    //var_dump($cleaned_AltText_Array);
+  
+  
+    $counter = 0;
+    $resultset = "";
+    foreach($cleaned_AltText_Array as $key=>$item){
+  
+      if($item == "Alt missing") {
+  
+        $resultset .= "<p>".$cleaned_Images_Array[$key]."</p>";
+        $counter++;
+  
+      }
+  
+    }
+    return [
+            "items"=>$resultset,
+            "counter"=>$counter
+          ];
+  }
