220a04d36857248fcac17fe18b1611640b24fac9
upzerk <86806059+upzerk@users.noreply.github.com>
2021-07-19 22:50:45 +0300

    nav

--- index.php
+++ index.php
@@ -20,6 +20,16 @@
 </head>
 
 <body>
+  <section class="container pt-2 pb-2">
+<ul class="nav nav-pills">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Report</a>
+  </li>
+</ul>
+</section>
   <section class="container">
     <table class="table">
       <tr>
